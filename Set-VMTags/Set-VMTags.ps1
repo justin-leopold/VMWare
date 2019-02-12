b3f8935002817c22d2903d22eede25698a469180
justin_leopold <justin_leopold@dpsk12.org>
2019-02-12 10:22:55 -0700

    roll back erroneous changes

--- Set-VMTags/Set-VMTags.ps1
+++ Set-VMTags/Set-VMTags.ps1
@@ -1,4 +1,3 @@
-Function Get-VMNotes {
 <#  
     .SYNOPSIS  
         Sets VMs to a certain tag. 
@@ -14,34 +13,34 @@ Function Get-VMNotes {
     .LINK  
     #>
 
-Class TagNames : System.Management.Automation.IValidateSetValuesGenerator {
-    [String[]] GetValidValues() {
-        $TagNames = (Get-Tag).Name
-                        
-        return [string[]] $TagNames
+    Class TagNames : System.Management.Automation.IValidateSetValuesGenerator {
+        [String[]] GetValidValues() {
+            $TagNames = (Get-Tag).Name
+                            
+            return [string[]] $TagNames
+        }
     }
-}
-
-    [CmdletBinding()]
-    param
-    (
-        #Virtual machine name
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
-        [string[]]$Vms,
+    Function Set-VMTags {
+        [CmdletBinding()]
+        param
+        (
+            #Virtual machine name
+            [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+            [string[]]$Vms,
+        
+            #vCenter Server Name
+            [Parameter(Mandatory)]
+            [ValidateSet('pdcvcenter', 'sdcvcenter')]
+            [string]$vcenterserver,
     
-        #vCenter Server Name
-        [Parameter(Mandatory)]
-        [ValidateSet('pdcvcenter', 'sdcvcenter')]
-        [string]$vcenterserver,
-
-        #Tags, class above gathers info
-        [ValidateSet([TagNames])]
-        [String]$Tag
-    )
-
-    $Tag = Get-Tag -Name Tier1
-
-    Foreach ($Vm in $VMs) {
-        New-TagAssignment -Tag $Tag -Entity $Vm -Server $vcenterserver
-    }
-} 
+            #Tags, class above gathers info
+            [ValidateSet([TagNames])]
+            [String]$Tag
+        )
+    
+        $Tag = Get-Tag -Name Tier1
+    
+        Foreach ($Vm in $VMs) {
+            New-TagAssignment -Tag $Tag -Entity $Vm -Server $vcenterserver
+        }
+    } 
